f8e9a28a3873275539ae9b71092af1a6ddd0dfe2
Question24x7 <question24x7.com@gmail.com>
2021-03-21 10:53:31 +0530

    Fronend issue solved

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var sio = io.connect("http://localhost:4000");
+var sio = io();
 var imgscreen = document.getElementById("imgscreen");
 var imgcam = document.getElementById("imgcam");
 sio.on("screencast", function(image){
